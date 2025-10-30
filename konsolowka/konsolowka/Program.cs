ac7c758fde60df4afc7c1ba865d4deae74e1c55c
mbubula6 <mbubula6@gmail.com>
2025-10-30 08:13:03 +0100

    Classes defined

--- konsolowka/konsolowka/Program.cs
+++ konsolowka/konsolowka/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hi");
+    }
+}
+class Urzadzenie
+{
+    public void Komunikat(string komunikat)
+    {
+        Console.WriteLine(komunikat);
+    }
+}
+class Pralka : Urzadzenie
+{
+    private int numerProgramuPrania = 0;
+    public int ustawNumerProgramuPrania(int danyNumerProgramuPrania)
+    {
+        if ( danyNumerProgramuPrania > 0 & danyNumerProgramuPrania < 12 )
+        {
+            numerProgramuPrania = danyNumerProgramuPrania;
+        } else
+        {
+            numerProgramuPrania = 0;
+        }
+        return numerProgramuPrania;
+    }
+
+}
+class Odkurzacz : Urzadzenie
+{
+    private bool stanOdkurzacza = false;
+    public void on()
+    {
+        if (!stanOdkurzacza)
+        {
+            stanOdkurzacza = true;
+            Komunikat("Odkurzacz włączono");
+        }
+    }
+    public void off()
+    {
+        if (stanOdkurzacza)
+        {
+            stanOdkurzacza = false;
+            Komunikat("Odkurzacz wyłączono");
+        }
+    }
+}
